8b061dc83cadef9adacbdee7c6980f6c50557705
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-04-10 14:59:07 +0200

    refactor: remove hello_world

--- rio_vrt/vrt.py
+++ rio_vrt/vrt.py
@@ -1,17 +1,16 @@
+"""rasterio based vrt creation."""
+
+import xml.etree.cElementTree as ET
 from pathlib import Path
 from typing import Union
-import xml.etree.cElementTree as ET
 from xml.dom import minidom
-from os.path import relpath
 
 import rasterio as rio
 from rasterio.enums import ColorInterp
 
-def build_vrt(
-    vrt_path: Union[str, Path],
-    files: list[Union[str, Path]]
-) -> Path:
-    """Create a vrt file from multiple files
+
+def build_vrt(vrt_path: Union[str, Path], files: list[Union[str, Path]]) -> Path:
+    """Create a vrt file from multiple files.
 
     Arguments:
         vrt_path: the final vrt file
@@ -20,7 +19,6 @@ def build_vrt(
     Returns:
         the path to the vrt file
     """
-
     # transform the final file in Path
     vrt_path = Path(vrt_path)
 
@@ -75,65 +73,85 @@ def build_vrt(
 
     # start the tree
     VRTDataset = ET.Element(
-        'VRTDataset',
-        {'rasterXSize': str(total_width),'rasterYSize': str(total_height)}
+        "VRTDataset",
+        {"rasterXSize": str(total_width), "rasterYSize": str(total_height)},
     )
-    ET.SubElement(VRTDataset, 'SRS', {'dataAxisToSRSAxisMapping': '2,1'}).text = crs.wkt
-    ET.SubElement(VRTDataset, "GeoTransform").text =", ".join([str(i) for i in transform.to_gdal()])
+    ET.SubElement(VRTDataset, "SRS", {"dataAxisToSRSAxisMapping": "2,1"}).text = crs.wkt
+    ET.SubElement(VRTDataset, "GeoTransform").text = ", ".join(
+        [str(i) for i in transform.to_gdal()]
+    )
     ET.SubElement(VRTDataset, "overViewList", {"resampling": "nearest"}).text = "2 4 8"
 
     # create the bands subelements
     VRTRasterBands = {}
     for i in indexes:
-        VRTRasterBands[i] = ET.SubElement(VRTDataset, "VRTRasterBand", {"datatype": dtypes[i-1], "band": str(i)})
-        if colorinterps[i-1] != ColorInterp.undefined:
-            ET.SubElement(VRTRasterBands[i], "ColorInterp").text = colorinterps[i-1].name.capitalize()
+        VRTRasterBands[i] = ET.SubElement(
+            VRTDataset, "VRTRasterBand", {"datatype": dtypes[i - 1], "band": str(i)}
+        )
+        if colorinterps[i - 1] != ColorInterp.undefined:
+            ET.SubElement(VRTRasterBands[i], "ColorInterp").text = colorinterps[
+                i - 1
+            ].name.capitalize()
 
     # add the files
     for f in files:
         relativeToVRT = "0" if f.is_absolute() else "1"
         with rio.open(f) as src:
             for i in indexes:
-                if colorinterps[i-1] == ColorInterp.alpha:
+                if colorinterps[i - 1] == ColorInterp.alpha:
                     source_type = "ComplexSource"
                 else:
-                    source_type= "SimpleSource"
+                    source_type = "SimpleSource"
 
                 Source = ET.SubElement(VRTRasterBands[i], source_type)
-                ET.SubElement(Source, "SourceFilename", {"relativeToVRT": relativeToVRT}).text = str(f)
+                ET.SubElement(
+                    Source, "SourceFilename", {"relativeToVRT": relativeToVRT}
+                ).text = str(f)
                 ET.SubElement(Source, "SourceBand").text = str(i)
-                ET.SubElement(Source, "SourceProperties", {
-                    "RasterXSize": str(src.width),
-                    "RasterYSize": str(src.height),
-                    "DataType": str(dtypes[i-1]),
-                    "BlockXSize": str(src.profile["blockxsize"]),
-                    "BlockYSize": str(src.profile["blockysize"]),
-                })
-                ET.SubElement(Source, "SrcRect", {
-                    "xOff":"0",
-                    "yOff":"0",
-                    "xSize": str(src.width),
-                    "ySize": str(src.height),
-                })
-                ET.SubElement(Source, "DstRect", {
-                    "xOff": str(abs(round((src.bounds.left - left)/x_res))),
-                    "yOff": str(abs(round((src.bounds.top - top)/y_res))),
-                    "xSize": str(src.width),
-                    "ySize": str(src.height),
-                })
-                if nodatavals[i-1] is not None:
-                    ET.SubElement(Source, "NoDataValue").text = str(nodatavals[i-1])
+                ET.SubElement(
+                    Source,
+                    "SourceProperties",
+                    {
+                        "RasterXSize": str(src.width),
+                        "RasterYSize": str(src.height),
+                        "DataType": str(dtypes[i - 1]),
+                        "BlockXSize": str(src.profile["blockxsize"]),
+                        "BlockYSize": str(src.profile["blockysize"]),
+                    },
+                )
+                ET.SubElement(
+                    Source,
+                    "SrcRect",
+                    {
+                        "xOff": "0",
+                        "yOff": "0",
+                        "xSize": str(src.width),
+                        "ySize": str(src.height),
+                    },
+                )
+                ET.SubElement(
+                    Source,
+                    "DstRect",
+                    {
+                        "xOff": str(abs(round((src.bounds.left - left) / x_res))),
+                        "yOff": str(abs(round((src.bounds.top - top) / y_res))),
+                        "xSize": str(src.width),
+                        "ySize": str(src.height),
+                    },
+                )
+                if nodatavals[i - 1] is not None:
+                    ET.SubElement(Source, "NoDataValue").text = str(nodatavals[i - 1])
                 ET.SubElement(Source, "Offset").text = "0.0"
                 ET.SubElement(Source, "Scale").text = "1.0"
 
-                if colorinterps[i-1] == ColorInterp.alpha:
+                if colorinterps[i - 1] == ColorInterp.alpha:
                     ET.SubElement(Source, "UseMaskBand").text = "true"
 
     # write the file
     vrt_path.write_text(
         minidom.parseString(ET.tostring(VRTDataset).decode("utf-8"))
         .toprettyxml(indent="  ")
-        .replace('&quot;', '"')
+        .replace("&quot;", '"')
     )
 
     return vrt_path
